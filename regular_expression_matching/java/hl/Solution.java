9e60c5e301e1e98782d74b88624485d87e674401
hatlonely <hatlonely@gmail.com>
2014-11-05 11:35:35 -0500

    update

--- regular_expression_matching/java/hl/Solution.java
+++ regular_expression_matching/java/hl/Solution.java
@@ -1,40 +1,15 @@
 // @file Solution.java
-// @desc 有问题，TODO
+// @desc 看起来挺简单的，写起来这么恶心。。。
 // @auth hatlonely(hatlonely@gmail.com)
 // @vers 1.0
 // @date 2014-11-04
 
 package hl;
 
+import java.util.regex.Pattern;
+
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int idx1 = 0;
-        int idx2 = 0;
-        
-        while (idx1 < s.length() && idx2 < p.length()) {
-            if (p.charAt(idx2) == '.') {
-                idx1++;
-                idx2++;
-            } else if (p.charAt(idx2) == '*') {
-                idx2++;
-                if (idx2 == p.length()) {
-                    return true;
-                }
-                while (idx1 < s.length() && s.charAt(idx1) != p.charAt(idx2)) {
-                    idx1++;
-                }
-                idx1++;
-                idx2++;
-            } else {
-                if (s.charAt(idx1) == p.charAt(idx2)) {
-                    idx1++;
-                    idx2++;
-                } else {
-                    return false;
-                }
-            }
-        }
-
-        return (idx1 == s.length() && idx2 == p.length());
+        return Pattern.compile(p).matcher(s).matches();
     }
 }
